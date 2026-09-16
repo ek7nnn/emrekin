75a236c24a177347f71cce3b17cbab00b28cd1a1
ek7nnn <kizgincelikekin@gmail.com>
2026-09-16 21:54:38 +0300

    tüm kodlar düzeltildi

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// NO runs away
-function dodge() {
-  noBtn.style.position = 'absolute';
-  noBtn.style.left = rand() + 'px';
-  noBtn.style.top = rand() + 'px';
-  yesScale += 0.07;
-}
-noBtn.on('enter', dodge);
+const noBtn = document.getElementById('noBtn');
+const yesBtn = document.getElementById('yesBtn');
+const mainTitle = document.getElementById('mainTitle');
 
-yesBtn.onclick = () => {
-  title.textContent = 'Yippeeee!';
-  rainHearts();
-};
+// "No" (kırmızı çarpı simgesi) butonunun fare yaklaşınca rastgele kaçması
+function moveNoButton() {
+    const randomX = (Math.random() - 0.5) * 150;
+    const randomY = (Math.random() - 0.5) * 100;
+    noBtn.style.transform = `translate(${randomX}px, ${randomY}px)`;
+}
+
+noBtn.addEventListener('mouseover', moveNoButton);
+noBtn.addEventListener('click', moveNoButton);
+
+// "Evet!" butonuna basıldığında yazının değişmesi
+yesBtn.addEventListener('click', () => {
+    mainTitle.textContent = 'Yippeeee! Seni Çok Seviyorum! ❤️';
+});
